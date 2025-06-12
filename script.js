70c82ef9124ed106e5054ec45b3238aed2e10d92
MatheusStoco <MSTOCO06@GMAIL.COM>
2025-06-12 20:22:02 -0300

    Início

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// Rolagem suave ao clicar nos links do menu
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
